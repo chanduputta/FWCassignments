6be2b8937f69909dee85637c0d6ca688edb1c93c
Shreyash Chandra <puttashreyash@gmail.com>
2022-09-16 12:09:09 +0530

    Rearranged script

--- script.sh
+++ script.sh
@@ -1,16 +1,20 @@
-#!/bin/bash                                 cd sdcard/Download/FWCassignments.git/trunk/AVR\ GCC\ assignment/code                   make                                        cd ..
+#!/bin/bash
+cd sdcard/Download/FWCassignments.git/trunk/AVR\ GCC\ assignment/code
+make
+cd ..
 pdflatex AVR-GCC.tex
 termux-open AVR-GCC.pdf &
 wait
 cd ..
 cd Assembly\ assigntment/code
-avra encoder.asm
-cd ..
-pdflatex Assembly.tex
+avra encoder.asm                       
+cd ..                                      
+pdflatex Assembly.tex                  
 termux-open Assembly.pdf &
 wait
 cd ..
-cd IDE\ assignment/code                     pio run
+cd IDE\ assignment/code
+pio run
 cd ..
 pdflatex IDE.tex
 termux-open IDE.pdf
